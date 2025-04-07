dd28459fef1f21f199c20918de24bf872857d95a
Alfo05 <9300387@alumnos.ufv.es>
2025-04-07 13:08:48 +0200

    Added menu

--- src/Program.cs
+++ src/Program.cs
@@ -6,6 +6,19 @@ namespace OOP
     {
         public static void Main()
         {
+            Console.WriteLine("_________________________________");
+            Console.WriteLine("|+++++++++++ Air UFV +++++++++++|");
+            Console.WriteLine("|-------------------------------|");
+            Console.WriteLine("| Choose an option              |");
+            Console.WriteLine("| 1. Load flight from file      |");
+            Console.WriteLine("| 2. Load flight Manually       |");
+            Console.WriteLine("| 3. Start Simulation(Manual)   |");
+            Console.WriteLine("| 4. Start Simulation(Automatic)|");
+            Console.WriteLine("| 5. Exit                       |");
+            Console.WriteLine("|-------------------------------|");
+            Console.WriteLine("|+++++++++++++++++++++++++++++++|");
+            Console.Writeline("|-------------------------------|");
+
             
             // PRINT THE MANAGEMENT MENU 
             
