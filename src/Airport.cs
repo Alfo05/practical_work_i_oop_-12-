3190598771890318a12ee249797590fe2949ab4d
sammaadaniel30 <danielsamac@gmail.com>
2025-04-07 10:57:01 +0200

    Creates attributes for the Airport class

--- src/Airport.cs
+++ src/Airport.cs
@@ -3,6 +3,19 @@ namespace OOP
 {
     public class Airport
     {
-        
+        // Array for runways the airport has 
+        private Runway[] runways;
+
+        // List of aircrafts in the Airport or bound for the Airport 
+        private List<Aircraft> aircrafts;
+
+        public Airport() 
+        {
+            runways = new Runway[1]; // We create 2 runways  
+
+            aircrafts = new List<Aircraft>(); // We start to load the airplanes  
+        }
+
+
     }
 }
