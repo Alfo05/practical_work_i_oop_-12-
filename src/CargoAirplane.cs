0b2bd6f6391b27fef32b8fd0f623154d21b710d1
sammaadaniel30 <danielsamac@gmail.com>
2025-04-07 11:34:59 +0200

    Added attributes to the CargoAirplane class

---
+++ src/CargoAirplane.cs
@@ -0,0 +1,24 @@
+using System; 
+
+namespace OOP 
+{
+
+    public class CargoAirplane : Aircraft 
+    {
+        public double max_load = 0; 
+        public CargoAirplane(string id, AircraftState state, int distance, int speed, double fuelCapacity, double fuelConsumption, double currentFuel, double max_load) 
+        : base(id, state, distance, speed ,fuelCapacity, fuelConsumption, currentFuel)
+        {
+            this.id = id; 
+            this.State = state; 
+            this.distance = distance; 
+            this.speed = speed; 
+            this.fuelCapacity = fuelCapacity; 
+            this.fuelConsumption = fuelConsumption; 
+            this.currentFuel = currentFuel; 
+            this.max_load = max_load = 0; 
+
+        }
+
+    }
+}
