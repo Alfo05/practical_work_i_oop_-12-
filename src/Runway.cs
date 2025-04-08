942f62a88949e319c658aaf1f3d5d26626a1c61c
sammaadaniel30 <danielsamac@gmail.com>
2025-04-08 11:36:58 +0200

    Made changes on the behaviour on the Runway methods

--- src/Runway.cs
+++ src/Runway.cs
@@ -19,26 +19,34 @@ namespace OOP
 
     public Runway(string id)
     {
-        id = id; 
+        this.id = id; 
         runwayStatus = RunwayStatus.Free;  // By default the runway is free
         CurrentAircraft = null; // We start with runways with no planes
         TicksToFree = 3; // By default it takes 3 ticks to clear a runway 
     }
     public void LandingAircraft(Aircraft aircraft)
     {
-        if (runwayStatus == RunwayStatus.Free)
+        if (runwayStatus == RunwayStatus.Free) // Check if the runway is free 
         {
-            CurrentAircraft = aircraft;
-            runwayStatus = RunwayStatus.Ocupied;
-            TicksToFree = 0;
-            aircraft.AircraftState = AircraftState.Landing;
-            Console.WriteLine($"Aircraft {aircraft.Id} is landing on Runway {Id}");
+            
+            runwayStatus = RunwayStatus.Ocupied; // The runway is in use by the plane which is landing 
+            CurrentAircraft.State = Aircraft.AircraftState.Landing; // We assing the plane as landing 
+            Console.WriteLine($"Aircraft {aircraft.id} is landing on Runway {id}"); // We show the info 
 
         }
         else 
         {
-            Console.WriteLine($"{Id} is not free");
+            Console.WriteLine($"Runway number {id} is not free"); // We tell that the runway is in use by another plane
         }
+    }
+
+    public void ReleaseRunway()
+    {
+        CurrentAircraft.State = Aircraft.AircraftState.OnGround; // We assign the plane as OnGroud 
+        runwayStatus = RunwayStatus.Free; // We assign that the runway can now be used 
+        Console.WriteLine($"Runway {id} is now clear"); // We tell that the runway is now clear
+
+
     }
 
     }
