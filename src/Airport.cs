b2da747b9a8a53d1a9cf28af64340c917e591c53
sammaadaniel30 <danielsamac@gmail.com>
2025-04-08 11:24:18 +0200

    Added the menu for the user

--- src/Airport.cs
+++ src/Airport.cs
@@ -18,6 +18,24 @@ namespace OOP
         }
     }
 
+    public void PrintMenu() // Prints the management system for the user to select an option
+    {
+
+
+        Console.WriteLine("_________________________________");
+        Console.WriteLine("|+++++++++++ Air UFV +++++++++++|");
+        Console.WriteLine("|-------------------------------|");
+        Console.WriteLine("| Choose an option              |");
+        Console.WriteLine("| 1. Load flight from file      |");
+        Console.WriteLine("| 2. Load flight Manually       |");
+        Console.WriteLine("| 3. Start Simulation(Manual)   |");
+        Console.WriteLine("| 4. Start Simulation(Automatic)|");
+        Console.WriteLine("| 5. Exit                       |");
+        Console.WriteLine("|-------------------------------|");
+        Console.WriteLine("|+++++++++++++++++++++++++++++++|");
+        Console.WriteLine("|-------------------------------|");
+    }
+
 
     }
 }
