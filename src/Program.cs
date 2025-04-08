cbb349c91b9672f3d5ff71de29d6ca5d64b052ac
sammaadaniel30 <danielsamac@gmail.com>
2025-04-08 11:24:35 +0200

    Created instantation of the Airport

--- src/Program.cs
+++ src/Program.cs
@@ -6,21 +6,14 @@ namespace OOP
     {
         public static void Main()
         {
-            Console.WriteLine("_________________________________");
-            Console.WriteLine("|+++++++++++ Air UFV +++++++++++|");
-            Console.WriteLine("|-------------------------------|");
-            Console.WriteLine("| Choose an option              |");
-            Console.WriteLine("| 1. Load flight from file      |");
-            Console.WriteLine("| 2. Load flight Manually       |");
-            Console.WriteLine("| 3. Start Simulation(Manual)   |");
-            Console.WriteLine("| 4. Start Simulation(Automatic)|");
-            Console.WriteLine("| 5. Exit                       |");
-            Console.WriteLine("|-------------------------------|");
-            Console.WriteLine("|+++++++++++++++++++++++++++++++|");
-            Console.Writeline("|-------------------------------|");
+
+
+            Console.WriteLine("Welcome to the AIRUFV management system"); 
 
             
-            // PRINT THE MANAGEMENT MENU 
+            Airport airport = new Airport(0,0); // Creates the airport, variables to be defined later 
+            
+            airport.PrintMenu(); // Prints menu options
             
         }
     }
