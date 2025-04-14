4007e5c76d7c7e0e70995bb319474b8720cecfa6
sammaadaniel30 <danielsamac@gmail.com>
2025-04-14 15:20:01 +0200

    Added the GetStatus Method

--- src/Runway.cs
+++ src/Runway.cs
@@ -17,37 +17,51 @@ namespace OOP
             Ocupied
         }
 
-    public Runway(string id)
-    {
-        this.id = id; 
-        runwayStatus = RunwayStatus.Free;  // By default the runway is free
-        CurrentAircraft = null; // We start with runways with no planes
-        TicksToFree = 3; // By default it takes 3 ticks to clear a runway 
-    }
-    public void LandingAircraft(Aircraft aircraft)
-    {
-        if (runwayStatus == RunwayStatus.Free) // Check if the runway is free 
+        public Runway(string id)
         {
+            this.id = id; 
+            runwayStatus = RunwayStatus.Free;  // By default the runway is free
+            CurrentAircraft = null; // We start with runways with no planes
+            TicksToFree = 3; // By default it takes 3 ticks to clear a runway 
+        }
+        public void LandingAircraft(Aircraft aircraft)
+        {
+            if (runwayStatus == RunwayStatus.Free) // Check if the runway is free 
+            {
             
-            runwayStatus = RunwayStatus.Ocupied; // The runway is in use by the plane which is landing 
-            CurrentAircraft.State = Aircraft.AircraftState.Landing; // We assing the plane as landing 
-            Console.WriteLine($"Aircraft {aircraft.id} is landing on Runway {id}"); // We show the info 
+                runwayStatus = RunwayStatus.Ocupied; // The runway is in use by the plane which is landing 
+                CurrentAircraft.State = Aircraft.AircraftState.Landing; // We assing the plane as landing 
+                Console.WriteLine($"Aircraft {aircraft.id} is landing on Runway {id}"); // We show the info 
 
+            }
+            else 
+            {
+                Console.WriteLine($"Runway number {id} is not free"); // We tell that the runway is in use by another plane
+            }
         }
-        else 
+
+        public void ReleaseRunway()
         {
-            Console.WriteLine($"Runway number {id} is not free"); // We tell that the runway is in use by another plane
+            CurrentAircraft.State = Aircraft.AircraftState.OnGround; // We assign the plane as OnGroud 
+            runwayStatus = RunwayStatus.Free; // We assign that the runway can now be used 
+            Console.WriteLine($"Runway {id} is now clear"); // We tell that the runway is now clear
+
+
         }
-    }
 
-    public void ReleaseRunway()
-    {
-        CurrentAircraft.State = Aircraft.AircraftState.OnGround; // We assign the plane as OnGroud 
-        runwayStatus = RunwayStatus.Free; // We assign that the runway can now be used 
-        Console.WriteLine($"Runway {id} is now clear"); // We tell that the runway is now clear
+        public string GetStatus()
+        {
+            if (runwayStatus == RunwayStatus.Free) // If the runway status is free
+            {
+                return $"{id}: IS FREE";
+            }
 
+            else // If the runway status is anything but free
+            {
+                return $"{id}: Runway is ocupied by Aircraft {CurrentAircraft.id}, {TicksToFree} remaining";
+            }
+}   
 
-    }
 
     }
 
